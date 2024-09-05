3d21118c4b4212f7c6ded50c192c13e9054d40b8
Grenish rai <ariestheracer@gmail.com>
2024-09-06 03:18:06 +0530

    Added NextJs

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+    <h1 className="text-5xl suse">Hi, welcome to Formo</h1>
+    <h1 className="text-5xl suse">Create and Share</h1>
+    </>
+  );
+}
